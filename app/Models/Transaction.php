c0882d816fc434b307a3fcb5ac2970d762390b23
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-04 20:41:29 +0700

    DONE model for transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'food_id', 'user_id', 'quantity', 'total', 'status', 'payment_url'
+    ];
+
+    //For table relationship
+    public function food(){
+        return $this->hasOne(Food::class, 'id', 'food_id');
+
+    }
+
+    public function user(){
+        return $this->hasOne(User::class, 'id', 'user_id');
+        
+    }
+
+    //Asesor for changing date format
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
 }
