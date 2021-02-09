75608085da4359264e742ca23b8c688407ac4763
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-09 21:08:32 +0700

    Added checkout midtrans

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -6,7 +6,10 @@ use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class TransactionController extends Controller
 {
@@ -64,5 +67,61 @@ class TransactionController extends Controller
 
         //and then give the updated data as a return
         return ResponseFormatter::success($transaction, 'Transaction updated successfully');
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'food_id' => 'required|exists:food,id',
+            'user_id' => 'required|exists:users,id',
+            'quantity' => 'required',
+            'total' => 'required',
+            'status' => 'required'
+        ]);
+
+        $transaction = Transaction::create([
+            'food_id' => $request->food_id,
+            'user_id' => $request->user_id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => $request->status,
+            'payment_url' => '', 
+        ]);
+
+        //Configure Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //Call transaction that already created
+        $transaction = Transaction::with(['food', 'user'])->find($transaction->id);
+
+        //Create Midtrans Transaction
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => $transaction->id,
+                'gross_amount' => (int) $transaction->total
+            ],
+            'customer_details' => [
+                'first_name' => $transaction->user->name,
+                'email' => $transaction->user->email,
+            ],
+            'enabled_payments' => ['gopay', 'bank_transfer'],
+            'vtweb' => []
+        ];
+        //Call Midtrans
+        try{
+            //Get first page midtrans payment
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+
+            $transaction->payment_url = $paymentUrl;
+            $transaction->save();
+
+            //Return data to API
+            return ResponseFormatter::success($transaction, 'Transaction Successful');
+        }catch(Exception $e){
+            return ResponseFormatter::error($e->getMessage(), 'Transaction Failed');
+        }
     }
 }
